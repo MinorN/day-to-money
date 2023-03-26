173579ac579d2ac8fac6c092e562ec4d9ed73c38
qijiajun <1481700559@qq.com>
2023-03-26 08:20:22 +0000

    添加tsx

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/day-to-money/dist', // build path
-  plugins: [vue()]
+  // base:'/day-to-money/dist', // build path
+  plugins: [
+    vue(),
+    vueJsx({
+      transformOn: true,
+      mergeProps: true
+    })
+  ]
 })
