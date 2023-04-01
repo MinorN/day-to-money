ceac0eee610ba8844707b3b932f18152bc4d1909
qijiajun <1481700559@qq.com>
2023-04-01 10:15:52 +0000

    创建overlay组件

--- src/shared/Icon.tsx
+++ src/shared/Icon.tsx
@@ -1,15 +1,19 @@
-import { defineComponent } from 'vue';
+import { PropType, defineComponent } from 'vue';
 import s from './Icon.module.scss';
 
 export const Icon = defineComponent({
   props: {
     name: {
       type: String,
+      required: true,
+    },
+    onClick: {
+      type: Function as PropType<(e: MouseEvent) => void>,
     },
   },
   setup: (props, context) => {
     return () => (
-      <svg class={s.icon}>
+      <svg class={s.icon} onClick={props.onClick}>
         <use xlinkHref={'#' + props.name}></use>
       </svg>
     );
