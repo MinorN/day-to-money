71b7accd10a6b1884ad4c5dad2028f7e220ff15f
qijiajun <1481700559@qq.com>
2023-04-02 03:48:13 +0000

    抽离MainLayout组件

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -7,6 +7,7 @@ import s from './StartPage.module.scss';
 import { Navbar } from '../shared/Navbar';
 import { Overlay } from '../shared/Overlay';
 import { RouterLink } from 'vue-router';
+import { MainLayout } from '../layouts/MainLayout';
 export const StartPage = defineComponent({
   setup: (props, context) => {
     const overlayVisible = ref(false);
@@ -15,28 +16,32 @@ export const StartPage = defineComponent({
     };
     return () => (
       <div>
-        <Navbar>
+        <MainLayout>
           {{
-            default: () => '山竹记账',
+            title: () => '山竹记账',
             icon: () => (
               <Icon name="menu" class={s.navIcon} onClick={onClickMenu} />
             ),
+            default: () => (
+              <>
+                <Center class={s.pig_wrapper}>
+                  <Icon name="pig" class={s.pig} />
+                </Center>
+                <div class={s.button_wrapper}>
+                  <RouterLink to="/items/create">
+                    <Button class={s.button}>开始记账</Button>
+                  </RouterLink>
+                </div>
+                <RouterLink to="/items/create">
+                  <FloatButton name="add" />
+                  {overlayVisible.value && (
+                    <Overlay onClose={() => (overlayVisible.value = false)} />
+                  )}
+                </RouterLink>
+              </>
+            ),
           }}
-        </Navbar>
-        <Center class={s.pig_wrapper}>
-          <Icon name="pig" class={s.pig} />
-        </Center>
-        <div class={s.button_wrapper}>
-          <RouterLink to="/items/create">
-            <Button class={s.button}>开始记账</Button>
-          </RouterLink>
-        </div>
-        <RouterLink to="/items/create">
-          <FloatButton name="add" />
-          {overlayVisible.value && (
-            <Overlay onClose={() => (overlayVisible.value = false)} />
-          )}
-        </RouterLink>
+        </MainLayout>
       </div>
     );
   },
