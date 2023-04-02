69d6ce8a39ac6cdfb46b273bfab90235b71d79a8
qijiajun <1481700559@qq.com>
2023-04-02 03:57:39 +0000

    初步实现创建页面

--- src/components/item/ItemCreate.tsx
+++ src/components/item/ItemCreate.tsx
@@ -1,7 +1,23 @@
 import { defineComponent } from 'vue';
 import s from './ItemCreate.module.scss';
+import { MainLayout } from '../../layouts/MainLayout';
+import { Icon } from '../../shared/Icon';
 export const ItemCreate = defineComponent({
   setup: (props, context) => {
-    return () => <div>ItemCreate</div>;
+    return () => (
+      <div>
+        <MainLayout>
+          {{
+            title: () => '记一笔',
+            icon: () => <Icon name="back" class={s.navIcon} />,
+            default: () => (
+              <>
+                <div>xxx</div>
+              </>
+            ),
+          }}
+        </MainLayout>
+      </div>
+    );
   },
 });
--- src/views/ItemPage.tsx
+++ src/views/ItemPage.tsx
@@ -1,7 +1,10 @@
 import { defineComponent } from 'vue';
 import s from './ItemPage.module.scss';
+import { MainLayout } from '../layouts/MainLayout';
+import { Icon } from '../shared/Icon';
+import { RouterView } from 'vue-router';
 export const ItemPage = defineComponent({
   setup: (props, context) => {
-    return () => <div class={s.itemPage}>ItemPage</div>;
+    return () => <RouterView />;
   },
 });
